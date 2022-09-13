7b4d0fb64eb5c5f67624c08dfe9afd6829efcb72
Arghya <arghyadutta080@gmail.com>
2022-09-13 15:26:30 +0530

    more buttons are added and trigonometic operation's display are modified

--- index.js
+++ index.js
@@ -6,47 +6,79 @@ Array.from(buttons).forEach((btn) => {
     btn.addEventListener('click', (element) => {
         // console.log(element.target)
         e = element.target.innerHTML
-        if (e != "=" && e != "ON" && e != "OFF" && e != "DEL" && e != "sin" && e != "cos" && e != "tan" && e != "pi" && e != "x^y") {
+
+        const calculation = (num) => {
+            if (num == 0 || num == 1) {
+                return 1
+            }
+            else return num*calculation(num - 1)
+        }
+
+        const factorial = (fact_str) =>{
+            fact_str.split("").reverse().join("")
+            fact_str = parseInt(fact_str)
+            ans = calculation(fact_str)
+            return ans
+        }
+
+        if (e != "=" && e != "ON" && e != "OFF" && e != "DEL" && e != "x^y" && e != "x!" && e != "√") {
             str = str + e
             document.querySelector('textarea').value = str
         }
+
         else if (e == "ON") {
             str = ""
             document.querySelector('textarea').value = "0"
         }
+
         else if (e == "OFF") {
             str = ""
             document.querySelector('textarea').value = str
         }
+
         else if (e == "DEL") {
             str = str.slice(0, -1)
             document.querySelector('textarea').value = str
         }
-        else if (e == "pi") {
-            pi = "Math.PI"
-            str = str + pi
-            document.querySelector('textarea').value = str
-        }
-        else if (e == "sin") { 
-            trigo = "Math.sin"
-            str = str + trigo
-            document.querySelector('textarea').value = str
-        }
-        else if (e == "cos") { 
-            trigo = "Math.cos"
-            str = str + trigo
-            document.querySelector('textarea').value = str
-        }
-        else if (e == "tan") { 
-            trigo = "Math.tan"
-            str = str + trigo
-            document.querySelector('textarea').value = str
-        }
+
         else if (e == "x^y") {
-            str = str + "**"
+            str = str + "^"
             document.querySelector('textarea').value = str
         }
+
+        else if (e == "x!") {
+            str = str + "!"
+            document.querySelector('textarea').value = str
+            char = ""
+            blank = ""
+            for (let i = str.length - 2; i >= 0; i--) {
+                if (str[i] >= '0' && str[i] <= '9') {
+                    char = char + str[i]
+                }
+                else break;
+            }
+            blank = blank + char.split("").reverse().join("") + "!"
+            // console.log(blank)
+            x = factorial(char)
+            new_str = x.toString();
+            // console.log(new_str)             
+        }
+
+        else if (e == "√") {
+            str = str + "√("
+            document.querySelector('textarea').value = str
+        }
+
         else if (e == "=") {
+            str = str.replace("sin", "Math.sin")
+            str = str.replace("cos", "Math.cos")
+            str = str.replace("tan", "Math.tan")
+            str = str.replace("pi", "Math.PI")
+            str = str.replace("e", "Math.E")
+            str = str.replace("ln", "Math.log")
+            str = str.replace(blank, x)
+            str = str.replace("√", "Math.sqrt")
+            str = str.replace("^", "**")
             str = eval(str)
             document.querySelector('textarea').value = str
         }
